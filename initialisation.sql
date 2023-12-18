a2e8099a5e0ae4aca4cd9c30780b22b4f8a2dadd
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2023-12-18 16:29:00 +0100

    Modification de la base de donnees et de orleans.html

--- initialisation.sql
+++ initialisation.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS batiment_service;
+DROP TABLE IF EXISTS services;
 DROP TABLE IF EXISTS batiments;
 
 CREATE TABLE batiments (
+    Id SERIAL PRIMARY KEY,
     Composante VARCHAR(255),
     Nom VARCHAR(255),
     Campus VARCHAR(255),
@@ -11,6 +14,23 @@ CREATE TABLE batiments (
     Geometrie GEOGRAPHY(Point, 4326)
 );
 
-\COPY batiments FROM 'batiments.csv' WITH CSV HEADER;
+\COPY batiments(Composante, Nom, Campus, CP, Rue, Coordonnees_lat, Coordonnees_lon, Geometrie) FROM 'batiments.csv' WITH CSV HEADER;
 
 UPDATE batiments SET Geometrie = ST_SetSRID(ST_MakePoint(Coordonnees_lon, Coordonnees_lat), 4326);
+
+CREATE TABLE services (
+    Id SERIAL PRIMARY KEY,
+    Nom_service VARCHAR(255),
+    Description_service TEXT,
+    Public_cible VARCHAR(255)
+);
+
+\COPY services(Nom_service, Description_service, Public_cible) FROM 'services.csv' WITH CSV HEADER;
+
+CREATE TABLE batiment_service (
+    Batiment_id INTEGER REFERENCES batiments(Id),
+    Service_id INTEGER REFERENCES services(Id),
+    PRIMARY KEY (Batiment_id, Service_id)
+);
+
+\COPY batiment_service(Batiment_id, Service_id) FROM 'batiment_service.csv' WITH CSV HEADER;
