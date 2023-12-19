248337eb9e58833fc2012d660b4c7f2c87474295
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2023-12-19 12:18:56 +0100

    Ajout de Bourges et de boutons de filtre

--- signodejs/main.js
+++ signodejs/main.js
@@ -19,6 +19,46 @@ const osm = new TileLayer({
     source: new OSM(),
 });
 
+document.getElementById("bourges").addEventListener("click", function() {
+    loadData('campus', 'Bourges').then(r => {
+
+    });
+});
+document.getElementById("orleans").addEventListener("click", function() {
+    loadData('nom', 'EGS').then(r => {});
+});
+
+export async function loadData(propriete,value) {
+
+    try {
+        const url = buildUrlFilter(propriete, value);
+
+        // Faites la requête pour obtenir les nouvelles données
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Mettez à jour les données du vectorLayer
+        vectorLayer.getSource().clear();
+
+        // Assurez-vous de spécifier la projection si elle n'est pas incluse dans le GeoJSON
+        const features = new format.GeoJSON().readFeatures(data, {
+            dataProjection: 'EPSG:4326', // Projection des données (peut varier selon votre cas)
+            featureProjection: 'EPSG:3857', // Projection pour affichage sur la carte (peut varier selon votre cas)
+        });
+
+        vectorLayer.getSource().addFeatures(features);
+
+        // Redessinez la couche vectorielle
+        vectorLayer.getSource().changed();
+
+        // Vous pouvez également ajuster le centrage/zoom de la carte si nécessaire
+        // map.getView().fit(vectorLayer.getSource().getExtent());
+
+    } catch (error) {
+        console.error('Erreur lors du chargement des données:', error);
+    }
+}
+
 const vectorLayer = new VectorLayer({
     source: new VectorSource({
         url: 'http://localhost:8080/geoserver/projet/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=projet%3Abatiments&maxFeatures=50&outputFormat=application%2Fjson',
@@ -38,107 +78,106 @@ const vectorLayer = new VectorLayer({
     }),
 });
 
-const map = new Map({
-    target: 'map',
-    layers: [osm, vectorLayer],
-    view: new View({
-        center: fromLonLat([1.934, 47.844]),
-        zoom: 17,
-    }),
-});
+    const map = new Map({
+        target: 'map',
+        layers: [osm, vectorLayer],
+        view: new View({
+            center: fromLonLat([1.934, 47.844]),
+            zoom: 17,
+        }),
+    });
 
-const highlightSource = new VectorSource();
-const highlightLayer = new VectorLayer({
-    source: highlightSource,
-    style: new Style({
-        image: new Circle({
-            radius: 8,
-            fill: new Fill({
-                color: 'yellow',
-            }),
-            stroke: new Stroke({
-                color: 'red',
-                width: 2,
+
+    const highlightSource = new VectorSource();
+    const highlightLayer = new VectorLayer({
+        source: highlightSource,
+        style: new Style({
+            image: new Circle({
+                radius: 8,
+                fill: new Fill({
+                    color: 'yellow',
+                }),
+                stroke: new Stroke({
+                    color: 'red',
+                    width: 2,
+                }),
             }),
         }),
-    }),
-});
+    });
 
-map.addLayer(highlightLayer);
+    map.addLayer(highlightLayer);
 
-const select = new Select({
-    layers: [vectorLayer],
-    condition: pointerMove,
-    style: new Style({
-        image: new Circle({
-            radius: 8,
-            fill: new Fill({
-                color: 'yellow',
-            }),
-            stroke: new Stroke({
-                color: 'red',
-                width: 2,
+    const select = new Select({
+        layers: [vectorLayer],
+        condition: pointerMove,
+        style: new Style({
+            image: new Circle({
+                radius: 8,
+                fill: new Fill({
+                    color: 'yellow',
+                }),
+                stroke: new Stroke({
+                    color: 'red',
+                    width: 2,
+                }),
             }),
         }),
-    }),
-});
+    });
 
-map.addInteraction(select);
+    map.addInteraction(select);
 
-select.on('select', event => {
-    highlightSource.clear();
-    if (event.selected.length > 0) {
-        highlightSource.addFeature(event.selected[0]);
-    }
-});
+    select.on('select', event => {
+        highlightSource.clear();
+        if (event.selected.length > 0) {
+            highlightSource.addFeature(event.selected[0]);
+        }
+    });
 
-const popup = new Overlay({
-    element: document.getElementById('popup'),
-    positioning: 'bottom-center',
-    offset: [0, -10],
-    autoPan: true,
-    autoPanAnimation: {
-        duration: 250,
-    },
-});
-map.addOverlay(popup);
-
+    const popup = new Overlay({
+        element: document.getElementById('popup'),
+        positioning: 'bottom-center',
+        offset: [0, -10],
+        autoPan: true,
+        autoPanAnimation: {
+            duration: 250,
+        },
+    });
+    map.addOverlay(popup);
 
 
-map.on('singleclick', event => {
-    const feature = map.forEachFeatureAtPixel(event.pixel, feature => feature);
+    map.on('singleclick', event => {
+        const feature = map.forEachFeatureAtPixel(event.pixel, feature => feature);
 
-    if (feature) {
-        const properties = feature.getProperties();
-        popup.setPosition(event.coordinate);
-        document.getElementById('popup').innerHTML = properties.nom + ' ' + properties.composante;
-        popup.getElement().style.display = 'block';
-        console.log('Propriétés de l\'entité :', properties.nom + feature.id_);
-        let id = feature.id_.replace('batiments.','');
+        if (feature) {
+            const properties = feature.getProperties();
+            popup.setPosition(event.coordinate);
+            document.getElementById('popup').innerHTML = properties.nom + ' ' + properties.composante;
+            popup.getElement().style.display = 'block';
+            console.log('Propriétés de l\'entité :', properties.nom + feature.id_);
+            let id = feature.id_.replace('batiments.', '');
 
-        requeteFilter('batiment_service', 'batiment_id', id)
-            .then(features => {
-                for(let i = 0; i < features.length; i++){
-                    const serviceId = features[i].getProperties().service_id;
-                    requeteId('services',serviceId).then((featureservice) => {
+            requeteFilter('batiment_service', 'batiment_id', id)
+                .then(features => {
+                    for (let i = 0; i < features.length; i++) {
+                        const serviceId = features[i].getProperties().service_id;
+                        requeteId('services', serviceId).then((featureservice) => {
 
-                        //TODO POUR MATTHIEU METTRE EN FORME + AJOUT DESCRIPTIF + AJOUT PUBLIC
+                            //TODO POUR MATTHIEU METTRE EN FORME + AJOUT DESCRIPTIF + AJOUT PUBLIC
 
-                        document.getElementById('popup').innerHTML += " " + featureservice[0].getProperties().nom_service;
-                    })
-                }
-            })
-            .catch(error => {
-                // Gérez les erreurs
-                console.error('Une erreur s\'est produite :', error);
-            });
-    } else {
-        document.getElementById('popup').innerHTML = '';
-        popup.getElement().style.display = 'none';
-    }
-});
+                            document.getElementById('popup').innerHTML += " " + featureservice[0].getProperties().nom_service;
+                        })
+                    }
+                })
+                .catch(error => {
+                    // Gérez les erreurs
+                    console.error('Une erreur s\'est produite :', error);
+                });
+        } else {
+            document.getElementById('popup').innerHTML = '';
+            popup.getElement().style.display = 'none';
+        }
+    });
 
-let featuresdata;
 async function requeteFilter(type, propriete, value) {
     try {
         // Generate a GetFeature request
@@ -201,3 +240,23 @@ async function requeteId(type, value) {
         console.error('Error in requete:', error);
     }
 }
+
+function buildUrlFilter(propriete,value) {
+    // Paramètres de base de l'URL
+    var urlBase = "http://localhost:8080/geoserver/projet/ows";
+    var service = "WFS";
+    var version = "1.0.0";
+    var request = "GetFeature";
+    var typeName = "projet:batiments";
+    var maxFeatures = 100;
+    var outputFormat = "application/json";
+
+    // Filtre CQL
+    var cqlFilter = "CQL_FILTER="+propriete+"%20=%20%27"+value+"%27";
+
+    // Construction de l'URL complète
+    var url = `${urlBase}?service=${service}&version=${version}&request=${request}&typeName=${typeName}&maxFeatures=${maxFeatures}&outputFormat=${outputFormat}&${cqlFilter}`;
+    console.log(url);
+    return url;
+}
+
